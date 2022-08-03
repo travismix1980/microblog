c72d248aed3bf13240df0205261b9824aa8fb107
Travis Mix <travismix1980@gmail.com>
2022-08-03 12:30:37 -0700

    added templates for base (inherited by most pages) and index.html.  Updated routes to pass through fake user and post data and title

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Travis'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Umatilla!',
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie sucked!',
+        },
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
